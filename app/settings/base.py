9fad8669d909907025b79bc69fd991e0793d73d3
p.minenkov <minikspb@gmail.com>
2025-02-09 20:33:18 +0300

    *

--- app/settings/base.py
+++ app/settings/base.py
@@ -169,9 +169,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/app/static"
+    BASE_DIR / "static"
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = 'login'
